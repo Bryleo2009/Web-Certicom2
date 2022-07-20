0056760f41f6aa77eb62f7c25f837ed3b95b9a8b
Bryleo <bryleo2009@hotmail.com>
2022-07-19 23:27:10 -0500

    first commit

---
+++ js/base.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // indicamos que se ejecuta la función a los 5 segundos de haberse
+    // cargado la pagina
+    
+    setInterval(clickbutton, 5000);/**5000 */ 
+  
+    function clickbutton() {
+      // simulamos el click del mouse en el boton del formulario
+      $("#boton").click();
+    }
+  });
+
+  window.addEventListener('touchstart', function(event) {
+    // some logic
+    event.preventDefault(); // <-- that should not be used in passive
+    // some other magic
+});
